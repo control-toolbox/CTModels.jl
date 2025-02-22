a1f2a5b12bb56f00bb6f454d1112813ffae60cfd
Olivier Cots <olivier.cots@irit.fr>
2025-02-22 22:50:15 +0100

    get from ctbase

---
+++ ext/CTBasePlots.jl
@@ -0,0 +1,15 @@
+module CTBasePlots
+
+#
+using DocStringExtensions
+using MLStyle # pattern matching
+
+#
+using CTBase
+using LinearAlgebra
+using Plots # redefine plot, plot!
+#import Plots: plot, plot!
+
+include("plot.jl")
+
+end
