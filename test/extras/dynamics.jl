6f1df615dc0896a8ea5f36bb12870853c1812e66
ocots <66357348+ocots@users.noreply.github.com>
2025-05-27 00:14:37 +0000

    :robot: Format .jl files

--- test/extras/dynamics.jl
+++ test/extras/dynamics.jl
@@ -7,11 +7,7 @@ partial_dyn_3!(r, t, x, u, v) = (@views r[1] .= x[1] + u[2])
 x = [1, 2, 3]
 u = [-1, 2]
 
-parts = [
-    (1:1, partial_dyn_1!),
-    (2:2, partial_dyn_2!),
-    (3:3, partial_dyn_3!),
-]
+parts = [(1:1, partial_dyn_1!), (2:2, partial_dyn_2!), (3:3, partial_dyn_3!)]
 
 dyn! = CTModels.__build_dynamics_from_parts(parts)
 
@@ -23,7 +19,6 @@ r - [x[1] + 2u[2], 2x[3], x[1] + u[2]]
 ###
 
 r[1:1] .= 0
-
 
 x = [1, 2, 3]
 x[1] = 10
