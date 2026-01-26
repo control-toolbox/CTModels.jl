6435abe36c36d00d80b8d2d7c97047f4e97aea9a
Olivier Cots <olivier.cots@irit.fr>
2026-01-26 23:05:13 +0100

    fix: Add missing exports and imports to InitialGuess module
    - Export validate_initial_guess from InitialGuess
    - Import state, control, variable from parent to extend them
    - Add documentation explaining why state/control/variable are not exported
    - These functions add methods to existing CTModels functions

--- src/InitialGuess/InitialGuess.jl
+++ src/InitialGuess/InitialGuess.jl
@@ -30,6 +30,9 @@ using CTBase
 import ..AbstractOptimalControlProblem, ..AbstractSolution
 import ..state_dimension, ..control_dimension, ..variable_dimension
 
+# Import functions to extend with new methods
+import ..state, ..control, ..variable
+
 # Import utilities from Utils module
 import ..Utils: ctinterpolate, matrix2vec
 
@@ -40,8 +43,13 @@ include("types.jl")
 include("initial_guess.jl")
 
 # Export public API
-export initial_guess, pre_initial_guess, build_initial_guess
+export initial_guess, pre_initial_guess, build_initial_guess, validate_initial_guess
 export OptimalControlInitialGuess, OptimalControlPreInit
 export AbstractOptimalControlInitialGuess, AbstractOptimalControlPreInit
+
+# Note: state, control, variable are NOT exported here as they are already
+# defined in the parent CTModels module for Model and Solution types.
+# The InitialGuess module defines additional methods for OptimalControlInitialGuess
+# which extend the existing functions.
 
 end
